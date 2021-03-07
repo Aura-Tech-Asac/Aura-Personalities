1032f94f6580c47b45d4683c6078511cf5afee5b
Mousa Sabah <mousa@DESKTOP-KQRJ9RV.localdomain>
2021-03-08 01:02:27 +0200

    Creat Quiz Page And Script

--- js/app.js
+++ js/app.js
@@ -39,10 +39,109 @@ function removeCard( event ){
 
 
 }
-
-
-
-
+// Creat Obejcts
+let optionsText = ['I think I can quickly see and take advantage of new opportunities.',
+  'I can work well with a very wide range of people.',
+  'Producing ideas is one of my natural assets.',
+  'My ability rests in being able to draw people out whenever I detect they have something of value to contribute to group objectives.',
+  'My capacity to follow through has much to do with my personal effectiveness.',
+  'I am ready to face temporary unpopularity if it leads to worthwhile results in the end.',
+  'I can usually sense what is realistic and likely to work.',
+  'I can offer a reasoned case for alternative courses of action without introducing bias or prejudice.',
+  'I am not at ease unless meetings are well structured and controlled and generally well conducted.',
+  'I am inclined to be too generous towards others who have a valid viewpoint that has not been given a proper airing.',
+  'I have a tendency to talk too much once the group gets on to new ideas.',
+  'My objectives outlook makes it difficult for me to join in readily and enthusiastically with colleagues.',
+  'I am sometimes seen as forceful and authoritarian if there is a need to get something done.',
+  'I find it difficult to lead from the front, perhaps because I am overresponsive to group atmosphere.',
+  'I am apt to get too caught up in ideas that occur to me and so lose track of what is happening.',
+  'My colleagues tend to see me as worrying unnecessarily over detail and the possibility that things may go wrong.',
+  'I have an aptitude for influencing people without pressuring them.',
+  'My general vigilance prevents careless mistakes and omissions being made.',
+  'I am ready to press for action to make sure that the meeting does not waste time or lose sight of the main objective.',
+  'I can be counted on to contribute something original.',
+  'I am always ready to back a good suggestion in the common interest.',
+  'I am keen to look for the latest in new ideas and developments.',
+  'I believe my capacity for judgment can help to bring about the right decisions.',
+  'I can be called upon to see that all essential work is organised',
+  'I have a quiet interest in getting to know colleagues better.',
+  'I am not reluctant to challenge the views of others or to hold a minority view myself.',
+  'I can usually find a line of argument to refute unsound propositions.',
+  'I think I have a talent for making things work once a plan has to be put into operation.',
+  'I have a tendency to avoid the obvious and to come out with the unexpected.',
+  'I bring a touch of perfectionism to any job I undertake.',
+  'I am ready to make use of contacts outside the group itself.',
+  'While I am interested in all views I have no hesitation in making up my mind once a decision has to be made.',
+  'I enjoy analysing situations and weighing up all the possible choices.',
+  'I am interested in finding practical solutions to problems.',
+  'I like to feel I am fostering good working relationships.',
+  'I can have a strong influence on decisions.',
+  'I can meet people who may have something new to offer.',
+  'I can get people to agree on a necessary course of action.',
+  'I feel in my element where I can give a task my full attention.',
+  'I like to find a field that stretches my imagination.',
+  'I would feel like retiring to a corner to devise a way out of the impasse before developing a line.',
+  'I would be ready to work with the person who showed the most positive approach.',
+  'I would find some way of reducing the size of the task by establishing what different individuals might best contribute.',
+  'My natural sense of urgency would help to ensure that we did not fall behind schedule.',
+  'I believe I would keep cool and maintain my capacity to think straight.',
+  'I would retain a steadiness of purpose in spite of the pressures.',
+  'I would be prepared to take a positive lead if I felt the group was making no progress.',
+  'I would open up discussions with a view to stimulating new thoughts and getting something moving.',
+  'I am apt to show my impatience with those who are obstructing progress.',
+  'Others may criticise me for being too analytical & insufficiently intuitive.',
+  'My desire to ensure that work is properly done can hold up proceedings.',
+  'I tend to get bored rather easily and rely on one or two stimulating members to spark me off.',
+  'I find it difficult to get started unless the goals are clear.',
+  'I am sometimes poor at explaining and clarifying complex points that occur to me.',
+  'I am conscious of demanding from others the things I cannot do myself.',
+  'I hesitate to get my points across when I run up against real opposition.'];
+let SectionTitle = [
+  'What I believe I can contribute to a team',
+  'If I have a possible shortcoming in teamwork, it could be that',
+  'When involved in a project with other people',
+  'My characteristic approach to group work is that',
+  'I gain satisfaction in a job because',
+  'If I’m suddenly given a difficult task with limited time and unfamiliar people',
+  'With reference to the problems to which I am subject in working in groups',
+];
+function Personality( name, shortcutName ) {
+  this.name = name;
+  this.value = 0;
+  this.percentege = 0;
+  this.shortcutName = shortcutName;
+  Personality.all.push( this );
+}
+Personality.all = [];
+let PersonalityName = ['COMPANY WORKER', 'CHAIRPERSON', 'SHAPER', 'PLANT', 'RESOURCE INVESTIGATOR',
+  'MONITOR EVALUATOR', 'TEAM WORKER', 'COMPLETERFINISHER'];
+let PersonalityShortcutName = ['CW', 'CP', 'SH', 'PL', 'RI', 'ME', 'TW', 'CF'];
+for ( let index = 0; index < PersonalityShortcutName.length; index++ ) {
+  const PersonalityObject = new Personality( PersonalityName[index], PersonalityShortcutName[index] );
+}
+Section.all = [];
+function Section( SectionNumber, header, userAnswer, options, order ) {
+  this.SectionNumber = SectionNumber;
+  this.header = header;
+  this.options = options;
+  this.order = order;
+  this.userAnswer = [];
+  Section.all.push( this );
+}
+let zeroArray = new Array( 8 ).fill( 0 );
+let index = 0;
+for ( let i in SectionTitle ) {
+  let tempArray = [];
+  for ( let j in zeroArray ) {
+    tempArray.push( optionsText[index] );
+    index++;
+  }
+  const SectionObject = new Section( i + 1, SectionTitle[i], zeroArray, tempArray, 0 );
+}
+console.log( Section.all );
+localStorage.setItem( 'Personality', JSON.stringify( Personality.all ) );
+localStorage.setItem( 'Section', JSON.stringify( Section.all ) );
+// *********************************************************************
 
 
 
---
+++ js/quiz.js
@@ -0,0 +1,224 @@
+let answersArray = [0,0,0,0,0,0,0,0];
+let sectionNumber=0;
+
+function render( num ){
+
+
+  let sectionHeaderElement = document.querySelector( '.sectionHeader' );
+  sectionHeaderElement.textContent=Section.all[num].header;
+
+  let optionLabel = document.querySelectorAll( 'label.optionLabel' );
+  for ( let i = 0; i < optionLabel.length; i++ ) {
+    optionLabel[i].textContent=Section.all[num].options[i];
+  }
+
+
+}
+
+let sectionDivv =document.querySelectorAll( 'div.rating-system1' );
+render( 0 );
+
+// console.log(xx)
+for ( let z = 0; z < sectionDivv.length; z++ ) {
+  sectionDivv[z] .addEventListener( 'click',gg );
+
+}
+// tt.addEventListener('click', gg ,false);
+
+
+// console.log( aa );
+
+function gg( evt ) {
+
+  let targeted = evt.target;
+  console.log ( targeted );
+  let ques1=targeted.value;
+  switch ( targeted.parentElement.id ) {
+  case 'ratingA1':
+    answersArray[0]=parseInt( ques1 );
+    break;
+  case 'ratingA2':
+    answersArray[1]=parseInt( ques1 );
+    break;
+  case 'ratingA3':
+    answersArray[2]=parseInt( ques1 );
+    break;
+  case 'ratingA4':
+    answersArray[3]=parseInt( ques1 );
+    break;
+  case 'ratingA5':
+    answersArray[4]=parseInt( ques1 );
+    break;
+  case 'ratingA6':
+    answersArray[5]=parseInt( ques1 );
+    break;
+  case 'ratingA7':
+    answersArray[6]=parseInt( ques1 );
+    break;
+  case 'ratingA8':
+    answersArray[7]=parseInt( ques1 );
+    break;
+
+  }
+
+}
+
+
+let progress=0;
+// eslint-disable-next-line no-unused-vars
+function showMe () {
+
+  let chboxs = document.getElementsByName( 'checkA' );
+  let vis = 'none';
+  for( let i=0;i<chboxs.length;i++ ) {
+
+    let iddiv=`ratingA${i+1}`;
+
+    let starsNo = `rateA${i+1}`;
+    let ele = document.getElementsByName( starsNo );
+
+    if ( chboxs[i].checked ){
+      vis = 'inline-block';
+    } else{
+      vis='none';
+      answersArray[i]=0;
+      for( let j=0;j<ele.length;j++ ){
+        ele[j].checked = false;
+      }
+    }
+    document.getElementById( iddiv ).style.display = vis;
+
+
+  }
+
+}
+
+// eslint-disable-next-line no-unused-vars
+function next(){
+
+
+
+  let correctAnswer  = checkAnswers();
+  if( correctAnswer && sectionNumber <= 6 ){
+
+    if ( sectionNumber < 6 ) {
+      render( sectionNumber+1 );
+    }
+
+    progress += 100/7 ;
+    let completedBar = document.querySelector( 'div.progress' );
+    completedBar.style.width= progress +'%' ;
+    let textBar = document.getElementById( 'completed_text' );
+    textBar.textContent=`${Math.ceil( progress )} %`;
+
+    for ( let i = 0; i < 8; i++ ) {
+
+      let x = answersArray[i]/summarray( answersArray );
+      //
+      Section.all[sectionNumber].userAnswer[i] = ( x * 10 );
+    }
+
+
+    answersArray = [0,0,0,0,0,0,0,0];
+
+    let chboxs = document.getElementsByName( 'checkA' );
+    let vis = 'none';
+    for( let i=0;i<chboxs.length;i++ ) {
+
+
+      let iddiv=`ratingA${i+1}`;
+
+      let starsNo = `rateA${i+1}`;
+      let ele = document.getElementsByName( starsNo );
+      if ( chboxs[i].checked ){
+        chboxs[i].checked=false;
+        vis = 'none';
+        for( let j=0;j<ele.length;j++ ){
+          ele[j].checked = false;
+        }
+      } else{
+        vis='none';
+        answersArray[i]=0;
+        for( let j=0;j<ele.length;j++ ){
+          ele[j].checked = false;
+        }
+
+
+
+
+
+      }
+      document.getElementById( iddiv ).style.display = vis;
+    }
+
+    sectionNumber++;
+
+
+    if ( sectionNumber === 7 ) {
+      finalResult();
+    }
+
+
+
+
+
+
+  }
+  //   if ( sectionNumber === 6 ) {
+  //     progress += 100/7 ;
+  //     let completedBar = document.querySelector( 'div.progress' );
+  //     completedBar.style.width= progress +'%' ;
+  //     let textBar = document.getElementById( 'completed_text' );
+  //     textBar.textContent=`${Math.ceil( progress )} %`;
+
+  //     for ( let i = 0; i < 8; i++ ) {
+
+  //       let x = answersArray[i]/summarray( answersArray );
+  //       //
+  //       Section.all[sectionNumber].userAnswer[i] = ( x * 10 );
+  //     }
+
+
+  //     answersArray = [0,0,0,0,0,0,0,0];
+
+  //     result();
+
+  //   }
+
+
+}
+
+
+
+
+
+
+
+// helper functions
+function summarray ( array )
+{
+  let sum=0;
+  for ( let i = 0; i < array.length; i++ ) {
+    sum += array[i];
+
+  }
+  return sum;
+}
+
+function checkAnswers(){
+  if ( summarray( answersArray )=== 0 ) {
+    let warningDiv = document.querySelector( '.warningDiv' );
+    warningDiv.style.display='inline-block';
+    return false;
+  }else{
+    let warningDiv = document.querySelector( '.warningDiv' );
+    warningDiv.style.display='none';
+    return true;
+  }
+
+}
+function finalResult(){
+  window.location = '../result.html';
+
+}
+
