c3a6b3bee3ea590204af1e6757fec175550b84e3
Mousa Sabah <mousa@DESKTOP-KQRJ9RV.localdomain>
2021-03-09 00:51:47 +0200

    Finish Quiz Page Style .. And animate the counter

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let clicks = 57;
 getData();
-updateCounter();
+
 
 document.getElementById( 'testBtn' ).addEventListener( 'click', updateCounter );
 
@@ -31,7 +31,39 @@ function getData() {
 
 
 }
+function animateValue( id, start, end, duration ) {
+  // assumes integer values for start and end
 
+  let obj = document.getElementById( 'counter' );
+  let range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  let minTimer = 50;
+  // calc step time to show all interediate values
+  let stepTime = Math.abs( Math.floor( duration / range ) );
+
+  // never go below minTimer
+  stepTime = Math.max( stepTime, minTimer );
+
+  // get current time and calculate desired end time
+  let startTime = new Date().getTime();
+  let endTime = startTime + duration;
+  let timer;
+
+  function run() {
+    let now = new Date().getTime();
+    let remaining = Math.max( ( endTime - now ) / duration, 0 );
+    let value = Math.round( end - ( remaining * range ) );
+    obj.innerHTML = value;
+    if ( value == end ) {
+      clearInterval( timer );
+    }
+  }
+
+  timer = setInterval( run, stepTime );
+  run();
+}
+
+animateValue( 'value', 0, clicks, 3000 );
 
 
 
