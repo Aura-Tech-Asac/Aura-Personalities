a5262be5e3697276ed2c81bbbed8f843a01c74b5
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-03-09 16:32:49 +0200

    add comment page

---
+++ js/comment.js
@@ -0,0 +1,40 @@
+let data;
+
+function getComment(){
+  data =JSON.parse( localStorage.getItem( 'comment' ) );
+}
+getComment();
+
+function render(){
+
+  let dinamicDivElement=document.getElementById( 'dinamicReviews' );
+  for( let i in data ){
+    let divElement = document.createElement( 'div' );
+    dinamicDivElement.appendChild( divElement );
+    divElement.className='post';
+
+
+    let imgCommaElement = document.createElement( 'img' );
+    divElement.appendChild( imgCommaElement );
+    imgCommaElement.src='images/homePage/comma.JPG';
+    imgCommaElement.style.width='50px';
+
+
+
+    let pElement = document.createElement( 'p' );
+    pElement.textContent=data[i].comment;
+    pElement.className='width';
+    divElement.appendChild( pElement );
+
+    let imgElement = document.createElement( 'img' );
+    divElement.appendChild( imgElement );
+    imgElement.src='images/homePage/userFeedbackImg2.svg';
+    imgElement.style.width='100px';
+
+    let hElement = document.createElement( 'h3' );
+    hElement.textContent=data[i].name;
+    divElement.appendChild( hElement );
+  }
+
+}
+render();
--- js/result.js
+++ js/result.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function getData() {
-  const sectionData = JSON.parse( localStorage.getItem( 'Section' ) );
-  const personalityData = JSON.parse( localStorage.getItem( 'Personality' ) );
+  const sectionData = JSON.parse(localStorage.getItem('Section'));
+  const personalityData = JSON.parse(localStorage.getItem('Personality'));
   sectionAll = sectionData;
   personalityAll = personalityData;
 }
@@ -11,35 +11,35 @@ let personalityAll = [];
 getData();
 
 function result() {
-  for ( let i in sectionAll ) {
-    for ( let j in sectionAll[0].userAnswer ) {
-      if( sectionAll[i].userAnswer[j] > 0 ){
+  for (let i in sectionAll) {
+    for (let j in sectionAll[0].userAnswer) {
+      if (sectionAll[i].userAnswer[j] > 0) {
         personalityAll[sectionAll[i].order[j] - 1].value += sectionAll[i].userAnswer[j];
       }
     }
   }
-  for( let i in personalityAll ){
-    personalityAll[i].percentege = Math.round( ( personalityAll[i].value * 100 )/70 );
+  for (let i in personalityAll) {
+    personalityAll[i].percentege = Math.round((personalityAll[i].value * 100) / 70);
   }
 }
 
-let color=['#E5C828','#8F759F','#4fd39e','#4fd39e','#51a9ab','#8F749C','#808080','#8F759F'];
+let color = ['#E5C828', '#8F759F', '#4fd39e', '#4fd39e', '#51a9ab', '#8F749C', '#808080', '#8F759F'];
 result();
-localStorage.setItem( 'resultpersonality', JSON.stringify( personalityAll ) );
-function renderResult(){
-  for( let i in personalityAll ){
-    let BarTitleElement= document.querySelectorAll( '.barTitle a p' );
+localStorage.setItem('resultpersonality', JSON.stringify(personalityAll));
+function renderResult() {
+  for (let i in personalityAll) {
+    let BarTitleElement = document.querySelectorAll('.barTitle a p');
 
-    BarTitleElement[i].style.color=color[i];
-    BarTitleElement[i].textContent=personalityAll[i].name;
+    BarTitleElement[i].style.color = color[i];
+    BarTitleElement[i].textContent = personalityAll[i].name;
 
-    let percentageBarElement= document.querySelectorAll( '.personalityPercent' );
+    let percentageBarElement = document.querySelectorAll('.personalityPercent');
     percentageBarElement[i].style.backgroundColor = color[i];
-    percentageBarElement[i].style.width=`${personalityAll[i].percentege }%`;
+    percentageBarElement[i].style.width = `${personalityAll[i].percentege}%`;
 
-    let textPecentageElement= document.querySelectorAll( '.percent p' );
-    textPecentageElement[i].style.color=color[i];
-    textPecentageElement[i].textContent=`${personalityAll[i].percentege} %`;
+    let textPecentageElement = document.querySelectorAll('.percent p');
+    textPecentageElement[i].style.color = color[i];
+    textPecentageElement[i].textContent = `${personalityAll[i].percentege} %`;
 
   }
 }
@@ -48,3 +48,23 @@ renderResult();
 
 // let BarTitleElement= document.getElementById('p1_bar');
 //     BarTitleElement.textContent=personalityAll[0].name;
+
+function setComment() {
+
+  let name = document.getElementById('name').value;
+  let comment = document.getElementById('bodyText').value;
+  if (name && comment) {
+    const addcomment = new CommentSection(name, comment);
+
+    localStorage.setItem('comment', JSON.stringify(CommentSection.arr));
+    window.location = './test.html';
+  }
+}
+
+
+function CommentSection(name, comment) {
+  this.name = name;
+  this.comment = comment;
+  CommentSection.arr.push(this);
+}
+CommentSection.arr = [];
